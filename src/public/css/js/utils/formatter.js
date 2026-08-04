cd16d54d81aa43720865200fabb429ed2f68f142
nodewyegr-boop <nodewyegr@gmail.com>
2026-08-05 00:16:32 +0700

    Update formatter.js

--- src/public/css/js/utils/formatter.js
+++ src/public/css/js/utils/formatter.js
@@ -1,1 +1,10 @@
-.
+const Formatter = {
+    parseContent(text) {
+        if (!text) return '';
+        // Blue Link Formatter (https://...)
+        const urlRegex = /(https?:\/\/[^\s]+)/g;
+        return text.replace(urlRegex, (url) => {
+            return `<a href="${url}" target="_blank" class="discord-link">${url}</a>`;
+        });
+    }
+};

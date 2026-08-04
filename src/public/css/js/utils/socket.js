f6189c80bd281b6cb4b3691dac67604be446dae5
nodewyegr-boop <nodewyegr@gmail.com>
2026-08-05 01:21:39 +0700

    Update socket.js

--- src/public/css/js/utils/socket.js
+++ src/public/css/js/utils/socket.js
@@ -1,40 +1,32 @@
-
-// src/public/js/socket.js
 window.socket = io();
 
 document.addEventListener('DOMContentLoaded', () => {
-    // รอจนกว่าหน้าเว็บและไฟล์ JS ทุกไฟล์จะโหลดเสร็จสมบูรณ์
-    
+
     socket.on('connect', () => {
+        console.log('[Socket] Connected to server');
         const savedToken = localStorage.getItem('discord_token');
         if (savedToken) {
+            UI.showLoginLoading();
             socket.emit('req_login', { token: savedToken });
-        } else if (window.UI) {
+        } else {
             UI.showLoginScreen();
         }
     });
 
-    socket.on('login_success', () => {
-        if (window.UI) UI.hideLoginScreen();
+    socket.on('login_success', (data) => {
+        console.log('[Socket] Login success!');
+        UI.hideLoginScreen();
         socket.emit('req_initial_data');
     });
 
     socket.on('login_error', (errorMsg) => {
-        if (window.UI) UI.showLoginError(errorMsg);
+        console.error('[Socket] Login error:', errorMsg);
+        UI.showLoginError(errorMsg);
         localStorage.removeItem('discord_token');
     });
 
     socket.on('logout_success', () => {
         localStorage.removeItem('discord_token');
         location.reload();
-    });
-
-    socket.on('res_initial_data', (data) => {
-        if (data.me && window.Sidebar) Sidebar.renderUserPanel(data.me);
-        if (data.guilds && window.Sidebar) Sidebar.renderGuilds(data.guilds);
-    });
-
-    socket.on('discord_message_create', (msg) => {
-        if (window.Chat) Chat.appendMessage(msg);
     });
 });

d1b5573371cf947ef0701350e1ff317873de2a11
nodewyegr-boop <nodewyegr@gmail.com>
2026-08-05 19:00:37 +0700

    Create utils/logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+class AppLogger:
+    """ระบบบันทึก Log การทำงานสลับตามโมดูลอย่างเป็นระเบียบ"""
+
+    @staticmethod
+    def get_logger(name: str) -> logging.Logger:
+        logger = logging.getLogger(name)
+        if not logger.handlers:
+            logger.setLevel(logging.INFO)
+            handler = logging.StreamHandler(sys.stdout)
+            formatter = logging.Formatter(
+                "[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S"
+            )
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+        return logger
